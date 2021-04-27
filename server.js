51f6ce95d3745156d2d574c60c4607a284f52ef1
Aniket Arora <aniketarora@Anikets-MacBook-Air.local>
2021-04-28 01:04:31 +0530

    project

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -33,7 +33,13 @@ app.get('/api/products', (req, res) => {
 });*/
 //Serve static assests in production
 
-
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 
 
@@ -42,3 +48,4 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server running on PORT ${port}`);
 });
+ 
