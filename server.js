eb7c2e9f36ebc0d7ade0b50c20bd0c527b02067b
Aniket Arora <aniketarora@Anikets-MacBook-Air.local>
2021-04-28 13:15:18 +0530

    project

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const path = require('path');
-const dotenv = require("dotenv");
 
-dotenv.config();
 const app = express();
 //connecting mongoose
 connectDB();
