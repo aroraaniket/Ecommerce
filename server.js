008140096e906fd4a8cb61d1505f5e5052ccb55c
Aniket Arora <aniketarora@Anikets-MacBook-Air.local>
2021-04-28 11:52:36 +0530

    project

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const dotenv = require("dotenv");
 
 dotenv.config();
