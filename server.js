22846bb74c0a5289f63dec0fe99f3dc03c2c2440
Aniket Arora <aniketarora@Anikets-MacBook-Air.local>
2021-04-28 11:50:49 +0530

    project

--- server.js
+++ server.js
@@ -33,7 +33,13 @@ app.get('/api/products', (req, res) => {
 });*/
 //Serve static assests in production
 
-
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 
 const port = process.env.PORT || 5000;
