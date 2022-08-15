37edd91265872e96008382d4077aad2b8f255a61
Aniket Arora <aniketarora568@gmail.com>
2022-08-15 22:40:11 +0530

    changed gitignore

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`server running on PORT ${port}`);
